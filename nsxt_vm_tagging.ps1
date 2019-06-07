acd19bfa6e7458ba7931d6ee905d83bacf433f48
rutgerblom <rutger.blom@gmail.com>
2019-06-07 20:34:28 +0200

    Update nsxt_vm_tagging.ps1

--- nsxt_vm_tagging.ps1
+++ nsxt_vm_tagging.ps1
@@ -1,15 +1,16 @@
 # General variables
 $vcenter = Read-Host -Prompt "Enter vCenter FQDN/IP"
 $vccred = Get-Credential -Message "vCenter Credentials"
+$vccluster = Read-Host -Prompt "Enter the vSphere cluster to fetch virtual machines"
 $nsxmanager = Read-Host -Prompt "Enter NSX Manager FQDN/IP"
 $nsxcred = Get-Credential -Message "NSX Manager Credentials"
 $posturl = "https://$nsxmanager/api/v1/fabric/virtual-machines?action=update_tags"
 $newtag = Read-Host -Prompt "Enter tag to add"
 $newscope = Read-Host -Prompt "Enter the tag scope (optional)"
 
-# Connect to vCenter and fetch virtual machines
+# Connect to vCenter and fetch virtual machines. Adjust this query to fetch a subset of virtual machines.
 Connect-VIServer -Server $vcenter -Credential $vccred
-$vms = Get-Cluster Cluster | Get-VM | ForEach-Object { $_ | Get-View }
+$vms = Get-Cluster $vccluster | Get-VM | ForEach-Object { $_ | Get-View }
 
 # Tag the fetched virtual machines
 foreach ($vm in $vms) {
