f55989fc734f302e0db177a753734345639eb226
Rutger Blom <serbl@SE-LND-SERBL.local>
2019-06-08 11:57:22 +0200

    renamed script

---
+++ nsxt_add_vm_tag.ps1
@@ -0,0 +1,31 @@
+# General variables
+$vcenter = Read-Host -Prompt "Enter vCenter FQDN/IP"
+$vccred = Get-Credential -Message "vCenter Credentials"
+$vccluster = Read-Host -Prompt "Enter the vSphere cluster name to fetch virtual machines"
+$nsxmanager = Read-Host -Prompt "Enter NSX Manager FQDN/IP"
+$nsxcred = Get-Credential -Message "NSX Manager Credentials"
+$posturl = "https://$nsxmanager/api/v1/fabric/virtual-machines?action=update_tags"
+$newtag = Read-Host -Prompt "Enter tag to add"
+$newscope = Read-Host -Prompt "Enter the tag scope (optional)"
+
+# Connect to vCenter and fetch virtual machines. Adjust this query to fetch a subset of virtual machines.
+Connect-VIServer -Server $vcenter -Credential $vccred
+$vms = Get-Cluster $vccluster | Get-VM | ForEach-Object { $_ | Get-View }
+
+# Tag the fetched virtual machines
+foreach ($vm in $vms) {
+    $vmid = $vm.Config.InstanceUuid
+    $vmname = $vm.Config.Name
+    $geturl = "https://$nsxmanager/api/v1/fabric/virtual-machines?external_id=$vmid&included_fields=tags"
+    $getrequest = Invoke-RestMethod -Uri $geturl -Authentication Basic -Credential $nsxcred -Method Get -ContentType "application/json" -SkipCertificateCheck
+    $getresult = $getrequest.results | ConvertTo-Json -Compress
+    $currenttags = [regex]::match($getresult,'\[([^\)]+)\]').Groups[1].Value
+
+    $JSON = @"
+    {"external_id":"$vmid","tags":[{"scope":"$newscope","tag":"$newtag"},$currenttags]}
+"@
+  Invoke-RestMethod -Uri $posturl -Authentication Basic -Credential $nsxcred -Method Post -Body $JSON -ContentType "application/json" -SkipCertificateCheck
+  Write-Host "$vmname tagged"
+}
+
+Write-host "All VMs tagged with tag $newtag $newscope"
