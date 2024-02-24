a19cb2c5b5d2dc7ebd8a13e0213c6a83290ef0eb
rutgerblom <rutger.blom@gmail.com>
2024-02-24 11:52:36 +0100

    convert to using environment variables

--- bash/README.md
+++ bash/README.md
@@ -9,17 +9,17 @@ Using an Ubuntu 22.04 machine:
 * ```chmod +x ~/git/nsx-t/bash/import_apply_certificate.sh```
 * Copy the certificate (chain) and its key to a working directory on the Ubuntu machine
 
-Edit ```~/git/nsx-t/bash/import_apply_certificate.sh``` and update the values for the below variables so that these match your environment:
-* ```NSX_MANAGER``` 
-* ```NSX_USER```
-* ```NSX_PASSWORD```
-* ```NSX_CERTIFICATE_CHAIN```
-* ```NSX_CERTIFICATE_PRIVATE_KEY```
-* ```NSX_CERTIFICATE_DISPLAY_NAME```
+Export variables with the values that are relevant for your environment. For example:
+```export NSX_MANAGER="Pod-240-NSXT-LM.SDDC.Lab"``` 
+```export NSX_USER="admin"```
+```export NSX_PASSWORD="VMware1!VMware1!"```
+```export NSX_CERTIFICATE_CHAIN="/tmp/pod-240-nsxt-lm.crt"```
+```export NSX_CERTIFICATE_PRIVATE_KEY="/tmp/pod-240-nsxt-lm.key"```
+```export NSX_CERTIFICATE_DISPLAY_NAME="Pod-240-NSXT-LM"```
 
 ## Usage
 After the preparations are completed you simply run: ```~/git/nsx-t/bash/import_apply_certificate.sh```. 
-The script will leverage the NSX REST API to import the TLS certificate and then apply it to the NSX Manager nodes and VIP. 
+The script will leverage the NSX REST API to import the TLS certificate and then apply it to the NSX Manager nodes and the VIP. 
 
 ### Example workflow using easy-rsa CA
 1. Prepare an OpenSSL .cnf file. An example .cnf file can be found [here](nsx.cnf).
@@ -29,5 +29,5 @@ The script will leverage the NSX REST API to import the TLS certificate and then
 5. Sign the request: ```ea sign-req server pod-240-nsxt-lm```.
 6. Copy the certificate to a working directory: ```cat /home/vmware/easy-rsa/pki/issued/pod-240-nsxt-lm.crt | sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' > /tmp/pod-240-nsxt-lm.crt```.
 7. Append the root certificate to the certificate in the working directory: ```cat /home/vmware/easy-rsa/pki/ca.crt >> /tmp/pod-240-nsxt-lm.crt```.
-8. Update the variables in ```~/git/nsx-t/bash/import_apply_certificate.sh``` so that they match your environment and requirements.
+8. Export variables with the correcy values .
 9. Run ```~/git/nsx-t/bash/import_apply_certificate.sh``` to import and apply the TLS certificate to NSX Manager.
--- bash/import_apply_certificate.sh
+++ bash/import_apply_certificate.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Global Parameters -- Change these so that they match your environment
-NSX_MANAGER="Pod-240-NSXT-LM.SDDC.Lab"                 # FQDN or IP address of the NSX Manager
-NSX_USER="admin"
-NSX_PASSWORD="VMware1!VMware1!"
-NSX_CERTIFICATE_CHAIN="/tmp/pod-240-nsxt-lm.crt"       # This file must contain the entire certificate chain (i.e. certificate + intermediate + root)
-NSX_CERTIFICATE_PRIVATE_KEY="/tmp/pod-240-nsxt-lm.key"
-NSX_CERTIFICATE_DISPLAY_NAME="Pod-240-NSXT-LM"         # The name of the certificate as displayed in NSX Manager
-
-
-############################################### No need to make modifications beyond here ############################################################
-
-
 # Create API call that imports the TLS certificate to NSX Manager
 URI="/api/v1/trust-management/certificates?action=import"
 METHOD="POST"
@@ -28,14 +16,14 @@ cat >/tmp/body.json <<EOF
 EOF
 
 BODY=$(cat /tmp/body.json)
-RESPONSE=$(curl -v -k -X $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json" -d "$BODY")
+RESPONSE=$(curl -v -k $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json" -d "$BODY")
 CERT_ID=$(echo $RESPONSE | jq -r '.results[] | {id} | join(" ")')          # Save the NSX certificate ID for later use
 
 
 # Create API call that fetches NSX Manager node UUIDs (Fetches up to 3 NSX Manager node UUIDs, if you have less nodes that's fine)
 URI="/api/v1/cluster"
 METHOD="GET"
-RESPONSE=$(curl -v -k -X $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD")
+RESPONSE=$(curl -v -k $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD")
 NODE_UUID_01=$(echo $RESPONSE | jq -r '.nodes[0].node_uuid')
 NODE_UUID_02=$(echo $RESPONSE | jq -r '.nodes[1].node_uuid')
 NODE_UUID_03=$(echo $RESPONSE | jq -r '.nodes[2].node_uuid')
@@ -44,22 +32,22 @@ NODE_UUID_03=$(echo $RESPONSE | jq -r '.nodes[2].node_uuid')
 # Create API call that applies the TLS certificate on NSX Manager node 01
 URI="/api/v1/trust-management/certificates/$CERT_ID?action=apply_certificate&service_type=API&node_id=$NODE_UUID_01"
 METHOD="POST"
-RESPONSE=$(curl -v -k -X $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
+RESPONSE=$(curl -v -k $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
 
 
 # Create API call that applies the TLS certificate on NSX Manager node 02. If there isn't a node 02 this is no problem
 URI="/api/v1/trust-management/certificates/$CERT_ID?action=apply_certificate&service_type=API&node_id=$NODE_UUID_02"
 METHOD="POST"
-RESPONSE=$(curl -v -k -X $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
+RESPONSE=$(curl -v -k $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
 
 
 # Create API call that applies the TLS certificate on NSX Manager node 03. If there isn't a node 03 this is no problem
 URI="/api/v1/trust-management/certificates/$CERT_ID?action=apply_certificate&service_type=API&node_id=$NODE_UUID_03"
 METHOD="POST"
-RESPONSE=$(curl -v -k -X $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
+RESPONSE=$(curl -v -k $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
 
 
 # Create API call that applies the TLS certificate on NSX Manager VIP
 URI="/api/v1/trust-management/certificates/$CERT_ID?action=apply_certificate&service_type=MGMT_CLUSTER"
 METHOD="POST"
-RESPONSE=$(curl -v -k -X $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
+RESPONSE=$(curl -v -k $METHOD "https://$NSX_MANAGER$URI" -u "$NSX_USER:$NSX_PASSWORD" -H "content-type: application/json")
